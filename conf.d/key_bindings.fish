e9dbc20743efbe9a68a259c5aa64d035d7a021b1
Seong Yong-ju <sei40kr@gmail.com>
2018-11-18 09:39:46 +0900

    Fix key bindings

--- conf.d/key_bindings.fish
+++ conf.d/key_bindings.fish
@@ -1,7 +1,7 @@
 # key_bindings.fish
 # author: Seong Yong-ju <sei40kr@gmail.com>
 
-bind \ch __select__pane_or_backward_delete_char
-bind \cj __select__pane_or_execute
-bind \ck __select__pane_or_kill_line
-bind \cl __select__pane_or_clear
+bind \ch __select_left_pane_or_backward_delete_char
+bind \cj __select_below_pane_or_execute
+bind \ck __select_above_pane_or_kill_line
+bind \cl __select_right_pane_or_clear
